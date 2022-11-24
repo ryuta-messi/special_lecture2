73728c1cedbc0caf1a062da60126d07124c253e4
RYUTA ISHIKAWA <77151911+ryuta-messi@users.noreply.github.com>
2022-11-24 21:15:14 +0900

    Update test_CSVPrinter.py

--- tests/test_CSVPrinter.py
+++ tests/test_CSVPrinter.py
@@ -4,18 +4,18 @@ from speciallecture.CSVPrinter import CSVPrinter
 class TestCSVPrinter(unittest.TestCase):
 
     def test_read(self):
-        printer = CSVPrinter("./sample.csv")
+        printer = CSVPrinter("/home/runner/work/special_lecture2/special_lecture2/tests/sample.csv")
         l = printer.read()
         self.assertEqual(3,len(l))
 
     def test_read2(self):
-        printer = CSVPrinter("./sample.csv")
+        printer = CSVPrinter("/home/runner/work/special_lecture2/special_lecture2/tests/sample.csv")
         line = printer.read()
         self.assertSetEqual("bbb2",line[1][1])
 
     def test_read3(self):
         try:
-            printer = CSVPrinter('./sample.csv')
+            printer = CSVPrinter('/home/runner/work/special_lecture2/special_lecture2/tests/sample.csv')
             printer.read()
             unittest.TestCase.fail("This line should not be invoked")
         except FileNotFoundError as e:
